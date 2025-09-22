3349a7ccf17ef982db970c2d0d0dee46898f798f
Sumit Patel <sumit@docsvault.onmicrosoft.com>
2025-09-22 13:54:16 +0530

    - add ios and andrioid

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'my-ios-project',
-  webDir: 'dist'
+  server: {
+    url:'https://dev-app2.docsvault.com/'
+  }
 };
 
 export default config;
